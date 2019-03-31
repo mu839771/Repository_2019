ec4af74070bce639dc4c2f50c95ddb94d4c1cad7
mu839771 <30843668+mu839771@users.noreply.github.com>
2019-03-31 20:36:42 +0530

    Single Thread Executor example

---
+++ SingleThreadPoolDemo.java
@@ -0,0 +1,33 @@
+package com.executor;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class SingleThreadPoolDemo {
+	public static void main(String[] args) {
+
+		Runnable jobOne = () -> {
+			for (int i = 0; i < 10; i++) {
+
+				System.out.println(Thread.currentThread() + " : " + i);
+
+			}
+		};
+
+		Runnable jobTwo = () -> {
+
+			List<String> list = Arrays.asList("Mukesh", "Kumar", "Anil");
+
+			for (String s : list) {
+				System.out.println(Thread.currentThread() + " : " + s);
+			}
+		};
+
+		ExecutorService service = Executors.newSingleThreadExecutor();
+		service.submit(jobOne);
+		service.submit(jobTwo);
+		service.shutdown();
+	}
+}
